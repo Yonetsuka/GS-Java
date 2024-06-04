e745da16b201a4ca61454ddd2241dcf2999c4b45
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-06-04 13:49:39 -0300

    Create Pescador.java

---
+++ src/br/com/fiap/bean/Pescador.java
@@ -0,0 +1,25 @@
+package from br.com.fiap;
+/**
+*
+* @author Cássio
+*
+*/
+
+public class Pescador{
+  //variáveis
+  private int peixesPegos;
+  private bool temLicensa;
+  //gets e setters
+  public int getPeixesPegos(){
+    return peixesPegos;
+  }
+  public void setPeixesPegos(){
+    this.peixesPegos = peixesPegos;
+  }
+  public bool getTemLicensa(){
+    return temLicensa;
+  }
+  public void setTemLicensa(){
+    this.temLicensa = temLicensa;
+  }
+}
